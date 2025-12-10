252c87e796c7dc8090849ceeafed0bfd397d0528
theodoreromeos <theoromeos@gmail.com>
2025-12-10 13:10:34 +0200

    refactor for services - reduced the number of services

--- src/main/java/com/theodore/racingcore/services/RacingServiceImpl.java
+++ src/main/java/com/theodore/racingcore/services/RacingServiceImpl.java
@@ -2,32 +2,41 @@ package com.theodore.racingcore.services;
 
 import com.theodore.infrastructure.common.exceptions.AlreadyExistsException;
 import com.theodore.racingcore.entities.racing.Track;
+import com.theodore.racingcore.mappers.TrackMapper;
 import com.theodore.racingcore.models.racing.requests.CreateNewTrackRequestDto;
 import com.theodore.racingcore.models.racing.respones.TrackResponseDto;
+import com.theodore.racingcore.repositories.TrackRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 
 @Service
 public class RacingServiceImpl implements RacingService {
 
-    private final TrackService trackService;
+    private final TrackRepository trackRepository;
+    private final TrackMapper trackMapper;
 
-    public RacingServiceImpl(TrackService trackService) {
-        this.trackService = trackService;
+    public RacingServiceImpl(TrackRepository trackRepository, TrackMapper trackMapper) {
+        this.trackRepository = trackRepository;
+        this.trackMapper = trackMapper;
     }
 
     @Override
     public TrackResponseDto createNewTrack(CreateNewTrackRequestDto request) {
-        if (trackService.existsByNameAndCountry(request.trackName(), request.country())) {
+        if (trackRepository.existsByTrackNameIgnoreCaseAndCountry(request.trackName(), request.country())) {
             throw new AlreadyExistsException(request.trackName() + " already exists in " + request.country());
         }
-        Track newTrack = trackService.mapRequestToEntity(request);
-        Track savedTrack = trackService.saveNewTrack(newTrack);
-        return trackService.mapEntityToResponse(savedTrack);
+        Track newTrack = trackMapper.toEntity(request);
+        Instant now = Instant.now();
+        newTrack.setDateCreated(now);
+        newTrack.setDateUpdated(now);
+        Track savedTrack = trackRepository.save(newTrack);
+        return trackMapper.toResponse(savedTrack);
     }
 
     @Override
     public TrackResponseDto getTrack(Long id) {
-        var track = trackService.getTrackById(id);
-        return trackService.mapEntityToResponse(track);
+        var track = trackRepository.findById(id).orElseThrow();
+        return trackMapper.toResponse(track);
     }
 }
